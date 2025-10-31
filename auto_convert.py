b15c385d7ecac4ad6d5320acf39fb73ccc388581
𝒅𝒓𝒆𝒂𝒎𝒔 <mybeautifuladress@gmail.com>
2025-10-31 12:02:58 +0100

    Omg that's so cool

--- auto_convert.py
+++ auto_convert.py
@@ -5,11 +5,14 @@ import os
 
 import PIL.ImageMode
 
+
 # returns buffer (bytes), width, height
-def image_to_buffer(img_path:str, threshold:float = 0.5, resize:tuple[int, int] = None) -> tuple[bytes, int, int]:
+def image_to_buffer(
+    img_path: str, threshold: float = 0.5, resize: tuple[int, int] = None
+) -> tuple[bytes, int, int]:
     """
     Converts a bitmap image (JPG, PNG, etc.) to a byte buffer, translating each RGB pixel into a single white/black dot that can be displayed on an OLED display.
-    
+
     Parameters
     ----------
     img_path:str
@@ -25,10 +28,11 @@ def image_to_buffer(img_path:str, threshold:float = 0.5, resize:tuple[int, int] 
         - int: The width of the image.
         - int: The height of the image.
     """
-    
 
     # open image
-    i = PIL.Image.open(img_path).convert("RGB") # always open in RGB mode (don't handle RGBA like in PNG)
+    i = PIL.Image.open(img_path).convert(
+        "RGB"
+    )  # always open in RGB mode (don't handle RGBA like in PNG)
 
     # resize if desired
     if resize != None:
@@ -38,42 +42,40 @@ def image_to_buffer(img_path:str, threshold:float = 0.5, resize:tuple[int, int] 
     width, height = i.size
 
     # calculate the threshold. In other words, the average RGB value that the pixel has to be below (filled in with darkness) to be considered "on" and above to be considered "off"
-    thresholdRGB:int = min(max(int(round(threshold * 255, 0)), 0), 255)
-    
+    thresholdRGB: int = min(max(int(round(threshold * 255, 0)), 0), 255)
+
     # get a list of individual bits for each pixel (True is filled in, False is not filled in)
-    bits:list[bool] = []
+    bits: list[bool] = []
     for y in range(0, height):
         for x in range(0, width):
-            pix:tuple[int, int, int, int] = i.getpixel((x, y)) #[R,G,B,A]
-            
+            pix: tuple[int, int, int, int] = i.getpixel((x, y))  # [R,G,B,A]
+
             # determine, is this pixel solid (filled in BLACK) or not (filled in WHITE)?
-            filled:bool = False
-            avg:int = int(round((pix[0] + pix[1] + pix[2]) / 3, 0))
-            if avg >= thresholdRGB: # it is bright (so fill it with an on pixel)
+            filled: bool = False
+            avg: int = int(round((pix[0] + pix[1] + pix[2]) / 3, 0))
+            if avg >= thresholdRGB:  # it is bright (so fill it with an on pixel)
                 filled = True
 
             # add it to the list of bits
             bits.append(filled)
 
     # now that we have all the bits, chunk them by 8 and convert
-    BytesToReturn:bytearray = bytearray()
-    bit_collection_buffer:list[bool] = []
+    BytesToReturn: bytearray = bytearray()
+    bit_collection_buffer: list[bool] = []
     for bit in bits:
-
         # add it
         bit_collection_buffer.append(bit)
 
         # if we are now at 8, convert and append
         if len(bit_collection_buffer) == 8:
-
             # convert to 1's and 0's
-            AsStr:str = ""
+            AsStr: str = ""
             for bit in bit_collection_buffer:
                 if bit:
                     AsStr = AsStr + "1"
                 else:
                     AsStr = AsStr + "0"
-            
+
             # convert to byte
             b = int(AsStr, 2)
 
@@ -86,15 +88,21 @@ def image_to_buffer(img_path:str, threshold:float = 0.5, resize:tuple[int, int] 
     # return!
     return (bytes(BytesToReturn), width, height)
 
-def images_to_buffers(original_bitmaps_dir:str, output_dir:str, threshold:float = 0.5, resize:tuple[int, int] = None) -> None:
+
+def images_to_buffers(
+    original_bitmaps_dir: str,
+    output_dir: str,
+    threshold: float = 0.5,
+    resize: tuple[int, int] = None,
+) -> None:
     """Converts all bitmap images in a folder to a buffer in another file. Great for converting a group of bitmap images to various sizes, ready for display on SSD-1306."""
 
     for filename in os.listdir(original_bitmaps_dir):
         fullpath = os.path.join(original_bitmaps_dir, filename)
         converted = image_to_buffer(fullpath, resize=resize, threshold=threshold)
-        
+
         # trim off the ".png"
-        fn_only:str = filename[0:-4]
+        fn_only: str = filename[0:-4]
         result_path = os.path.join(output_dir, fn_only)
         f = open(result_path, "wb")
         f.write(bytes(converted[0]))
@@ -102,3 +110,34 @@ def images_to_buffers(original_bitmaps_dir:str, output_dir:str, threshold:float 
 
         # print
         print("Finished converting '" + filename + "'!")
+
+
+paths = [
+    "temp/clock/0.png",
+    "temp/clock/1.png",
+    "temp/clock/2.png",
+    "temp/clock/3.png",
+    "temp/clock/4.png",
+    "temp/clock/5.png",
+    "temp/clock/6.png",
+    "temp/clock/7.png",
+    "temp/clock/8.png",
+    "temp/clock/9.png",
+    "temp/clock/dots.png",
+]
+
+
+def convert():
+    for i in range(10):
+        print(i)
+        converted = image_to_buffer(paths[i])
+        print(paths[i])
+        print(converted[0])
+        print("\n")
+        print("\n")
+    # special traitement for ":" because it's not a number
+    converted = image_to_buffer(paths[10])
+    print("dots ", converted[0])
+
+convert()
+
